db60a0a11819627c42a9a59e33760276483f8664
Hassan Mourtada <hassanmourtada15@gmail.com>
2024-03-04 13:19:16 +0200

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Home from "./Pages/Home/Home";
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
@@ -7,7 +7,7 @@ import CardPage from './Pages/CardPage/CardPage';
 import CardDetails from './Components/CardDetails/CardDetails';
 import NotFound from './Components/NotFound/NotFound';
 import CardEdit from './Components/CardEdit/CardEdit';
-
+import NotFound from "./pages/notfound/NotFound";
 function App() {
   return (
     <Router>
@@ -18,7 +18,7 @@ function App() {
         <Route path="/cardpage" element={<CardPage />} />
         <Route path="/carddetails" element={<CardDetails />} />
         <Route path="/cardedit" element={<CardEdit />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );

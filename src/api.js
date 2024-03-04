f12b1f235a2aaf5a1e047b4281e5e1713993d0e5
Hassan Mourtada <hassanmourtada15@gmail.com>
2024-03-04 21:32:24 +0200

    changed render link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://final-project-backend-1-jccc.onrender.com',
+  baseURL: 'https://final-project-backend-fksz.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },

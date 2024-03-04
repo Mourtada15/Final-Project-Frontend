032a3441218fa527b9a3bda5743079c43f9b7da5
Hassan Mourtada <hassanmourtada15@gmail.com>
2024-03-04 17:38:26 +0200

    1

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://final-project-backend-dxkr.onrender.com',
+  // baseURL: 'https://final-project-backend-dxkr.onrender.com',
+  baseURL: 'https://final-project-backend-1-jccc.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
